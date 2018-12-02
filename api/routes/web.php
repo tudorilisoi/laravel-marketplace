7f23b94a58c3fe6c1e4e10dbbf8fbc2c69ddc37e
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-12-02 16:32:05 +0200

    activation

--- api/routes/web.php
+++ api/routes/web.php
@@ -9,15 +9,23 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
+use \App\User;
 
 Route::get('reset_password/{token}', ['as' => 'password.reset', function ($token) {
-    // implement your reset password route here!
+    // implement your reset password route here!   
     return response()->json($token);
 }]);
 
 Route::get('activate/{token}', ['as' => 'user.activation', function ($token) {
-    return response()->json($token);
+    $user = User::where(['activation_code' => $token])->first();
+    if (!$user) {
+        abort(400, 'This token does not exist');
+    }
+    $user->is_activated = 1;
+    $user->save();
+    return view('welcome');
+    // return response()->json($token);
 }]);
 
 Route::get('account_update/{token}', ['as' => 'user.account_update', function ($token) {
